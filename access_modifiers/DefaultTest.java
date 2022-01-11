b907d038747f71d2f9065feeb73d3ca0191d7da6
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2022-01-11 17:44:26 +0530

    Add files via upload

---
+++ access_modifiers/DefaultTest.java
@@ -0,0 +1,17 @@
+class DefaultTest
+{
+  void m1()
+  {
+    System.out.println("m1 method");
+  }
+  void m2()
+  {
+    System.out.println("m2 method");
+  }
+  public static void main(String[] args)
+  {
+     DefaultTest dt = new DefaultTest();
+     dt.m1();
+     dt.m2();
+   }
+}
