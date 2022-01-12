44744717119477d3eae492b52b8d1fba2ac843e0
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2022-01-12 12:25:10 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,14 @@
+class Ternary
+{
+  public static void main(String[] args)
+  {
+    /*int a =10;
+    int b = 20;
+    int a = 10;
+    int b = 10;*/
+    int a = 0;
+    int b = 1;
+    int x = (a>b)?a:b;
+    System.out.println("x is=" +x);
+  }
+}
