ab6df90b45ca4529a33089e7a5feb4e13d498a0d
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2022-01-12 17:00:57 +0530

    Add files via upload

---
+++ TryCatchcode.java
@@ -0,0 +1,20 @@
+class TryCatchcode
+{
+  public static void main(String[] args)
+  {
+    int a = 10;
+    int b = 5;
+    int c = 5;
+    int x,y;
+    try
+    {
+      x = a/(b-c);
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("Divided by zero");
+    }
+    y = a/(b+c);
+    System.out.println("y =" +y);
+ }
+}
