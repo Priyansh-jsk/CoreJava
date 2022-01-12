1ff0cfde7369aa95b086f0eae553087bda5b534d
Priyansh-jsk <58244081+Priyansh-jsk@users.noreply.github.com>
2022-01-12 17:09:52 +0530

    Add files via upload

--- MultiArea.java
+++ MultiArea.java
@@ -2,7 +2,7 @@ class Multi
 {
 	float length;
 	float breath;
-	void getdata(float a, float b)
+	void getdata(float a, float b)   
   	{
     	    length = a;
             breath = b;
@@ -13,7 +13,7 @@ class MultiArea
    public static void main(String[] args)
    {
 	float area;
-	Multi multi1 = new Multi();
+	Multi multi1 = new Multi(); //create an object multi1
 	multi1.getdata(7,10);
 	area = multi1.length * multi1.breath;
 	System.out.println("Area =" +area);
